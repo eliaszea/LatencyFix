c8c8bd048bf96772b1240b28fbecdf498043632e
Elias Zea <zea@kth.se>
2026-01-22 10:24:08 -0800

    Update manual.js

--- netlify/functions/manual.js
+++ netlify/functions/manual.js
@@ -1,9 +1,13 @@
-export default async () => \{\
-  const \{ MANUAL_URL \} = process.env;\
-\
-  if (!MANUAL_URL) \{\
-    return new Response("Missing MANUAL_URL", \{ status: 500 \});\
-  \}\
-\
-  return Response.redirect(MANUAL_URL, 302);\
-\};}
+// netlify/functions/manual.js
+export default async () => {
+  const { MANUAL_URL } = process.env;
+
+  if (!MANUAL_URL) {
+    return new Response("Missing MANUAL_URL", { status: 500 });
+  }
+
+  return new Response(null, {
+    status: 302,
+    headers: { Location: MANUAL_URL },
+  });
+};
